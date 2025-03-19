3e10e873a978703f6b54fe62be51d825af7535cd
albat <albatrin.demiraj@gmail.com>
2025-03-19 16:17:31 +0100

    Dopolnil funkcijo za zmansjanje slike

--- naloga1.py
+++ naloga1.py
@@ -2,8 +2,7 @@ import cv2 as cv
 import numpy as np
 
 def zmanjsaj_sliko(slika, sirina, visina):
-    '''Zmanjšaj sliko na velikost sirina x visina.'''
-    pass
+    return cv.resize(slika,(sirina,visina))
 
 def obdelaj_sliko_s_skatlami(slika, sirina_skatle, visina_skatle, barva_koze) -> list:
     '''Sprehodi se skozi sliko v velikosti škatle (sirina_skatle x visina_skatle) in izračunaj število pikslov kože v vsaki škatli.
@@ -12,6 +11,8 @@ def obdelaj_sliko_s_skatlami(slika, sirina_skatle, visina_skatle, barva_koze) ->
     Primer: Če je v sliki 25 škatel, kjer je v vsaki vrstici 5 škatel, naj bo seznam oblike
       [[1,0,0,1,1],[0,0,0,0,0],[0,0,0,0,0],[0,0,0,0,0],[1,0,0,0,1]]. 
       V tem primeru je v prvi škatli 1 piksel kože, v drugi 0, v tretji 0, v četrti 1 in v peti 1.'''
+   
+            
     pass
 
 def prestej_piklse_z_barvo_koze(slika, barva_koze) -> int:
